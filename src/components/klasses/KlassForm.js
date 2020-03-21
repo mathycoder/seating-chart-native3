e408335283f39459b22c7f94ad7b5a9a3a25806c
Adam Sarli <adam.sarli@gmail.com>
2020-03-21 17:29:55 -0400

    styled new dropdown

--- src/components/klasses/KlassForm.js
+++ src/components/klasses/KlassForm.js
@@ -3,7 +3,7 @@ import { addKlass, updateKlass, deleteKlass } from '../../actions/klassActions.j
 import { connect } from 'react-redux'
 import { View, Text, StyleSheet, FlatList,
         TouchableOpacity, TextInput, Picker } from 'react-native'
-import RNPickerSelect from 'react-native-picker-select';
+import RNPickerSelect, { defaultStyles }  from 'react-native-picker-select';
 
 const KlassForm = ({ displayFormSet, klass }) => {
   const [name, setName] = useState('')
@@ -18,22 +18,56 @@ const KlassForm = ({ displayFormSet, klass }) => {
 
   return (
     <View style={styles.rowStyle}>
-      <RNPickerSelect
-        onValueChange={(value) => setPeriod(value)}
-        items={[
-          { label: '1', value: 1 },
-          { label: '2', value: 2 },
-          { label: '3', value: 3 },
-          { label: '4', value: 4 },
-          { label: '5', value: 5 },
-          { label: '6', value: 6 },
-          { label: '7', value: 7 },
-          { label: '8', value: 8 },
-          { label: '9', value: 9 }
-        ]}
-        placeholder={{label: "Period"}}
-        value={period}
-      />
+      <View style={styles.periodColumn}>
+        <RNPickerSelect
+          onValueChange={(value) => setPeriod(value)}
+          items={[
+            { label: '1', value: 1 },
+            { label: '2', value: 2 },
+            { label: '3', value: 3 },
+            { label: '4', value: 4 },
+            { label: '5', value: 5 },
+            { label: '6', value: 6 },
+            { label: '7', value: 7 },
+            { label: '8', value: 8 },
+            { label: '9', value: 9 }
+          ]}
+          placeholder={{
+            label: "P",
+            value: null,
+            color: 'gray'
+          }}
+          value={period}
+          style={{
+                ...pickerSelectStyles,
+                iconContainer: {
+                  top: 20,
+                  right: 8,
+                },
+                placeholder: {
+                  color: 'black',
+                  fontSize: 12,
+                },
+              }}
+              Icon={() => {
+                    return (
+                      <View
+                        style={{
+                          backgroundColor: 'transparent',
+                          borderTopWidth: 7,
+                          borderTopColor: 'rgb(121, 124, 132)',
+                          borderRightWidth: 7,
+                          borderRightColor: 'transparent',
+                          borderLeftWidth: 7,
+                          borderLeftColor: 'transparent',
+                          width: 0,
+                          height: 0
+                        }}
+                      />
+                    );
+                  }}
+        />
+      </View>
       <TextInput
         style={styles.textInput}
         autoCorrect={false}
@@ -54,8 +88,10 @@ const styles = StyleSheet.create({
     marginVertical: 8
   },
   periodColumn: {
-    width: 70,
-    textAlign: "center"
+    width: 50,
+    marginHorizontal: 15,
+    textAlign: "center",
+    alignItems: "center"
   },
   nameColumn: {
     width: 120,
@@ -70,15 +106,22 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: 120,
     fontSize: 18
-  },
-  pickerStyle: {
-    height: 20,
-    width: 40,
-  },
-  pickerText: {
-    fontSize: 18,
   }
 })
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    backgroundColor: "rgb(81,84,92)",
+    fontSize: 18,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 4,
+    color: 'white',
+    paddingRight: 20, // to ensure the text is never behind the icon
+  }
+});
 
 function mapDispatchToProps(dispatch){
   return {
